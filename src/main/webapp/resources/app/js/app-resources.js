ff8dd859e505fa307076517a877581470b08f2e8
Kazuki Shimizu <kazuki43zoo@gmail.com>
2014-07-12 18:15:09 +0900

    Refactoring the javascript.

--- src/main/webapp/resources/app/js/app-resources.js
+++ src/main/webapp/resources/app/js/app-resources.js
@@ -1,12 +1,16 @@
-'use strict';
+(function() {
+	'use strict';
 
-// --------
-// Models
-// --------
-var resources = angular.module('resources', [ 'ngResource' ]);
-var contextPath = angular.element("meta[name='contextPath']").attr('content');
-resources.factory("DateResource", [ '$resource', function($resource) {
-	return $resource(contextPath + '/api/v1/dates/:dateId', {
-		todoId : "@dateId"
-	});
-} ]);
+	// --------
+	// Models
+	// --------
+	var resources = angular.module('resources', [ 'ngResource' ]);
+	var contextPath = angular.element("meta[name='contextPath']").attr(
+			'content');
+	resources.factory("DateResource", [ '$resource', function($resource) {
+		return $resource(contextPath + '/api/v1/dates/:dateId', {
+			todoId : "@dateId"
+		});
+	} ]);
+
+})();
--- src/main/webapp/resources/app/js/app.js
+++ src/main/webapp/resources/app/js/app.js
@@ -1,3 +1,4 @@
-'use strict';
-
-var app = angular.module("app", [ "resources" ]);
+(function() {
+	'use strict';
+	var app = angular.module("app", [ "resources" ]);
+})();
