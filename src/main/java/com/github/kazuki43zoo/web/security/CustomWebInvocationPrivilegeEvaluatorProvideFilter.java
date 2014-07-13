c04ffff15b22b7f32de83315ba8f2acf7d71ce2f
Kazuki Shimizu <kazuki43zoo@gmail.com>
2014-07-13 14:51:45 +0900

    Modified way to provide the WebInvocationPrivilegeEvaluator.

---
+++ src/main/java/com/github/kazuki43zoo/web/security/CustomWebInvocationPrivilegeEvaluatorProvideFilter.java
@@ -0,0 +1,80 @@
+package com.github.kazuki43zoo.web.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.access.WebInvocationPrivilegeEvaluator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomWebInvocationPrivilegeEvaluatorProvideFilter implements Filter,
+        WebInvocationPrivilegeEvaluator, BeanPostProcessor {
+
+    private List<WebInvocationPrivilegeEvaluator> webInvocationPrivilegeEvaluators = new ArrayList<>();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        request.setAttribute(WebAttributes.WEB_INVOCATION_PRIVILEGE_EVALUATOR_ATTRIBUTE, this);
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        webInvocationPrivilegeEvaluators.clear();
+    }
+
+    @Override
+    public boolean isAllowed(String uri, Authentication authentication) {
+        for (WebInvocationPrivilegeEvaluator privilegeEvaluator : webInvocationPrivilegeEvaluators) {
+            if (!privilegeEvaluator.isAllowed(uri, authentication)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean isAllowed(String contextPath, String uri, String method,
+            Authentication authentication) {
+        for (WebInvocationPrivilegeEvaluator privilegeEvaluator : webInvocationPrivilegeEvaluators) {
+            if (!privilegeEvaluator.isAllowed(contextPath, uri, method, authentication)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName)
+            throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+        if (bean instanceof WebInvocationPrivilegeEvaluator
+                && !bean.getClass().isAssignableFrom(getClass())) {
+            webInvocationPrivilegeEvaluators.add((WebInvocationPrivilegeEvaluator) bean);
+        }
+        return bean;
+    }
+
+}
