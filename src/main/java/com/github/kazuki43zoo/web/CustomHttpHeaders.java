21003fad9dad49de508b95ca1c6605c5858b1e54
Kazuki Shimizu <kazuki43zoo@gmail.com>
2014-07-20 16:02:45 +0900

    Added ther CustomHttpHeaders.java to define the custom header name.

---
+++ src/main/java/com/github/kazuki43zoo/web/CustomHttpHeaders.java
@@ -0,0 +1,9 @@
+package com.github.kazuki43zoo.web;
+
+public final class CustomHttpHeaders {
+    private CustomHttpHeaders() {
+        // NOP
+    }
+
+    public static final String X_TRACK = "X-Track";
+}
