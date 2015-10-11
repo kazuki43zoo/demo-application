11dd4e3ac199438437d5c795d1c2cc247ef3ff8d
Kazuki Shimizu <kazuki43zoo@gmail.com>
2015-10-12 03:45:32 +0900

    Polishing travis-ci

---
+++ startup-cargo-daemon.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export CARGO_DAEMON_WEBAPP_VERSION=1.4.16
+if test ! -f $HOME/cargo-daemon/cargo-daemon-webapp-${CARGO_DAEMON_WEBAPP_VERSION}.war ; then
+    mvn dependency:copy -Dartifact=org.codehaus.cargo:cargo-daemon-webapp:${CARGO_DAEMON_WEBAPP_VERSION}:war -DoutputDirectory=$HOME/cargo-daemon/.
+fi
+java -jar $HOME/cargo-daemon/cargo-daemon-webapp-${CARGO_DAEMON_WEBAPP_VERSION}.war &
