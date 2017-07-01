831ef71f9764ab261ec7606873d935976daf683c
Kazuki Shimizu <kazuki43zoo@gmail.com>
2017-07-02 00:10:48 +0900

    Change to use terasoluna-gfw-validator

--- src/main/java/com/github/kazuki43zoo/core/validation/EqualsPropertyValues.java
+++ src/main/java/com/github/kazuki43zoo/core/validation/EqualsPropertyValues.java
@@ -1,6 +1,9 @@
 package com.github.kazuki43zoo.core.validation;
 
+import org.terasoluna.gfw.common.validator.constraints.Compare;
+
 import javax.validation.Constraint;
+import javax.validation.OverridesAttribute;
 import javax.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -11,16 +14,23 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
-@Constraint(validatedBy = {EqualsPropertyValuesValidator.class})
+@Constraint(validatedBy = {})
 @Target({TYPE, ANNOTATION_TYPE})
 @Retention(RUNTIME)
+@Compare(left = "", right = "", operator = Compare.Operator.EQUAL, message = "{com.github.kazuki43zoo.core.validation.EqualsPropertyValues.message}")
 public @interface EqualsPropertyValues {
-    String message() default "{com.github.kazuki43zoo.core.validation.EqualsPropertyValues.message}";
+    String message() default "";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 
+    @OverridesAttribute(constraint = Compare.class, name = "left")
     String property();
+
+    @OverridesAttribute(constraint = Compare.class, name = "right")
     String comparingProperty();
+
+    @OverridesAttribute(constraint = Compare.class, name = "requireBoth")
+    boolean requireBoth() default false;
 
     @Target({TYPE, ANNOTATION_TYPE})
     @Retention(RUNTIME)
