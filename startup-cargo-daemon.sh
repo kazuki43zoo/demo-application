4330203f104c385c02ad55e16ba1b2b67697718a
Kazuki Shimizu <kazuki43zoo@gmail.com>
2017-07-01 18:39:59 +0900

    Upgrade to Cargo Maven Plugin 1.6.2

--- startup-cargo-daemon.sh
+++ startup-cargo-daemon.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export CARGO_DAEMON_WEBAPP_VERSION=1.4.19
+export CARGO_DAEMON_WEBAPP_VERSION=1.6.2
 if test ! -f $HOME/cargo-daemon/cargo-daemon-webapp-${CARGO_DAEMON_WEBAPP_VERSION}.war ; then
     mvn dependency:copy -Dartifact=org.codehaus.cargo:cargo-daemon-webapp:${CARGO_DAEMON_WEBAPP_VERSION}:war -DoutputDirectory=$HOME/cargo-daemon/.
 fi
