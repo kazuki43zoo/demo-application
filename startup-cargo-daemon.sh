b255c0f7c76023f5cad9793fc276aff30eae2876
Kazuki Shimizu <kazuki43zoo@gmail.com>
2018-02-03 20:45:29 +0900

    Update build plugin

--- startup-cargo-daemon.sh
+++ startup-cargo-daemon.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export CARGO_DAEMON_WEBAPP_VERSION=`mvn -B -f terasoluna-gfw-parent/pom.xml help:evaluate -Dexpression=cargo-maven2-plugin.version | grep -v '^\['`
+export CARGO_DAEMON_WEBAPP_VERSION=`mvn help:evaluate -Dexpression=cargo-maven2-plugin.version | grep -v '^\['`
 if test ! -f $HOME/cargo-daemon/cargo-daemon-webapp-${CARGO_DAEMON_WEBAPP_VERSION}.war ; then
     mvn dependency:copy -Dartifact=org.codehaus.cargo:cargo-daemon-webapp:${CARGO_DAEMON_WEBAPP_VERSION}:war -DoutputDirectory=$HOME/cargo-daemon/.
 fi
