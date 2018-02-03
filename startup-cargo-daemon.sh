8eb6932c53f7e3073d1f0c41841ce7179645635c
Kazuki Shimizu <kazuki43zoo@gmail.com>
2018-02-03 20:53:10 +0900

    Update build plugin

--- startup-cargo-daemon.sh
+++ startup-cargo-daemon.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-export CARGO_DAEMON_WEBAPP_VERSION=`mvn help:evaluate -Dexpression=cargo-maven2-plugin.version | grep -v '^\['`
+
+CARGO_DAEMON_WEBAPP_VERSION=`mvn help:evaluate -Dexpression=cargo-maven2-plugin.version | grep -v '^\['`
 if test ! -f $HOME/cargo-daemon/cargo-daemon-webapp-${CARGO_DAEMON_WEBAPP_VERSION}.war ; then
     mvn dependency:copy -Dartifact=org.codehaus.cargo:cargo-daemon-webapp:${CARGO_DAEMON_WEBAPP_VERSION}:war -DoutputDirectory=$HOME/cargo-daemon/.
 fi
