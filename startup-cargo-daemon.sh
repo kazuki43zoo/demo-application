3623b5fa9057112556fa26281acf354bb50c51eb
Kazuki Shimizu <kazuki43zoo@gmail.com>
2020-06-30 15:19:34 +0900

    fix syntax error of bash

--- startup-cargo-daemon.sh
+++ startup-cargo-daemon.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 CARGO_DAEMON_WEBAPP_VERSION=$(mvn help:evaluate -Dexpression=cargo-maven2-plugin.version | grep -v '^\[')
-if test ! -f $HOME/cargo-daemon/cargo-daemon-webapp-${CARGO_DAEMON_WEBAPP_VERSION}.war ; then
+if [ ! -f $HOME/cargo-daemon/cargo-daemon-webapp-${CARGO_DAEMON_WEBAPP_VERSION}.war ]; then
     mvn dependency:copy -Dartifact=org.codehaus.cargo:cargo-daemon-webapp:${CARGO_DAEMON_WEBAPP_VERSION}:war -DoutputDirectory=$HOME/cargo-daemon/.
 fi
 java -jar $HOME/cargo-daemon/cargo-daemon-webapp-${CARGO_DAEMON_WEBAPP_VERSION}.war &
